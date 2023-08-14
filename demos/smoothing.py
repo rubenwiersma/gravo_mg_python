cbe93a16d2a9dc4a2f31affdc8d60fda6e897bb4
rubenwiersma <r.t.wiersma@tudelft.nl>
2023-08-13 21:45:01 -0700

    Update demos

--- demos/smoothing.py
+++ demos/smoothing.py
@@ -8,16 +8,16 @@ from scipy import sparse
 # Viewer
 import polyscope as ps
 import polyscope.imgui as psim
-# Multigrid Solver
-from gravomg import MultigridSolver, Hierarchy, Sampling
-from gravomg.util import neighbors_from_stiffness, normalize_area, knn_undirected, neighbors_from_faces
 
-from robust_laplacian import mesh_laplacian, point_cloud_laplacian
+# Multigrid Solver
+from gravomg import MultigridSolver
+from gravomg.util import neighbors_from_stiffness, normalize_area
+
 # Experiment util
 from util import read_mesh
 
 # Read mesh
-V, F = read_mesh(Path('data/bimba.obj').resolve())
+V, F = read_mesh(Path('path_to_mesh.obj').resolve())
 N = igl.per_vertex_normals(V, F)
 
 print(f'Mesh loaded, {V.shape[0]} vertices')
@@ -33,9 +33,7 @@ Minv = sparse.diags(1 / M.diagonal())
 neigh = neighbors_from_stiffness(S)
 
 # Create reusable solver
-t = time.perf_counter()
-solver = MultigridSolver(V, neigh, M, verbose=True, ratio=8, lower_bound=1000, tolerance=1e-4, max_iter=100, sampling_strategy=Sampling.FASTDISK)
-print(f'Our construction time: {time.perf_counter() - t}')
+solver = MultigridSolver(V, neigh, M)
 
 # Start UI
 ui_tau = 0.001
@@ -53,7 +51,6 @@ def smoothing(tau):
 
     mesh.update_vertex_positions(mg_V)
 
-
 # GUI
 def smoothing_panel():
     global ui_tau
@@ -61,7 +58,7 @@ def smoothing_panel():
     psim.TextUnformatted("Smoothing")
     changed, ui_tau = psim.InputFloat("tau", ui_tau, format='%.6f')
 
-    if(psim.Button("Smoothe")):
+    if(psim.Button("Smooth")):
         smoothing(ui_tau)
 
 ps.init()
